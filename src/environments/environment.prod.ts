67b03a76a9ba84f1ebed572ce83e792d15f866ba
manfredwern <manfredwern@gmail.com>
2021-02-13 22:51:25 +0100

    added missing env keys to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,  
+  apodApi : {
+    key: '5B6oJsSCQyekXZvNOKpsUhRPl1e7FHqjIAyHpybk',
+    url: 'https://api.nasa.gov/planetary/apod?api_key='
+  }
 };
